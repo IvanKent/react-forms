7f4946388ef04028d4204c9ce400fddc91a022d2
IvanKent <ivankentmalate.landicho@bicol-u.edu.ph>
2024-01-19 11:31:34 +0800

    update the signUpForm

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -1,24 +1,28 @@
 import { useState } from "react"
 
 export default function SignUpForm(){
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [formData, setFormData] = useState({firstName: '', lastName: '', password: ''});
 
-    function updateFirstName (evt) {
-        setFirstName(evt.target.value)
-    }
-    function updateLastName (evt) {
-        setLastName(evt.target.value)
+    function updateFormData (evt) {
+        const changedField = evt.target.name;
+        const newValue = evt.target.value;
+        setFormData(currData => {
+            currData[changedField] = newValue;
+            return {...currData};
+        })
     }
     function print(){
-        console.log(firstName, lastName);
+        console.log(formData);
+
     }
     return (
         <div>
             <label htmlFor="firstName">FirstName: </label>
-            <input type="text" id="firstName" onChange={updateFirstName} value={firstName}/>
+            <input type="text" id="firstName" placeholder="first name" onChange={updateFormData} value={formData.firstName} name="firstName"/>
             <label htmlFor="lastName">LastName: </label>
-            <input type="text" id="lastName" onChange={updateLastName} value={lastName} />
+            <input type="text" id="lastName" placeholder="last name" onChange={updateFormData} value={formData.lastName} name="lastName"/>
+            <label htmlFor="password">Password: </label>
+            <input type="text" id="password" placeholder="password" onChange={updateFormData} value={formData.password} name="password" />
             <button onClick={print}>Submit</button>
         </div>
     )
